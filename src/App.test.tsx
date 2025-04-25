af253e14ab449ff0b1a3240a5d8979849d2eb87d
Ajit Singh <sajit5035@gmail.com>
2025-04-25 16:51:19 +0530

    github action added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,1 +1,10 @@
-export {}
+// App.test.js
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders the heading text', () => {
+  render(<App />);
+  const headingElement = screen.getByText(/this is react app for github action/i);
+  expect(headingElement).toBeInTheDocument();
+});
