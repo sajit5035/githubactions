6c1842e69467e9a40742223e5a5c17c8979c6b30
Ajit Singh <sajit5035@gmail.com>
2025-04-26 15:17:48 +0530

    github action run test case 15

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
     <div>
       <h1>This is react app for github action</h1>
       <h1>this is added</h1>
-      <h1>now i can deploy cc</h1>
+      <h1>now i can deploy ccx</h1>
     </div>
   )
 }
